f155a54643626a6be3b24f06eeba73b8ce573518
Gerhard Preuss <gelipp@gmail.com>
2013-12-17 17:09:17 +0100

    add echo example (not working yet)

---
+++ example/echo_client.lua
@@ -0,0 +1,21 @@
+#!/usr/bin/env lua
+local this_dir = arg[0]:match('(.+/)[^/]+%.lua') or './'
+package.path = this_dir..'../src/'..package.path
+
+local net = require'net'
+local ev = require'ev'
+
+local client = net.connect(12345)
+client:on('connect',function()
+    client:set_nodelay(true)
+    client:on('data',function(data)
+        print(data)
+      end)
+    local i = 0
+    ev.Timer.new(function()
+        print('writing',i)
+        client:write(tostring(i)..'\n')
+      end,1,1):start(ev.Loop.default)
+  end)
+
+net.loop()
---
+++ example/echo_server.lua
@@ -0,0 +1,16 @@
+#!/usr/bin/env lua
+local this_dir = arg[0]:match('(.+/)[^/]+%.lua') or './'
+package.path = this_dir..'../src/'..package.path
+
+local net = require'net'
+
+local server = net.listen(12345)
+server:on('connection',function(client)
+    client:set_nodelay(true)
+    client:on('data',function(data)
+        print(data)
+        client:write(data)
+      end)
+  end)
+
+net.loop()
