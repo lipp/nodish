7bb1b1c4229102e68a0bfb930edda5e39252624c
Gerhard Preuss <gelipp@gmail.com>
2013-12-27 22:18:01 +0100

    move default loop to process module

--- src/nodish/net.lua
+++ src/nodish/net.lua
@@ -1,32 +1,5 @@
 local socket = require'nodish.net.socket'
 local server = require'nodish.net.server'
-local ev = require'ev'
-
-local unloop = function()
-  print('quitting')
-  ev.Loop.default:unloop()
-end
-
-local loop = function()
-  local sigkill = 9
-  local sigint = 2
-  local sigquit = 3
-  local sighup = 1
-  for _,sig in ipairs({sigkill,sigint,sigquit,sighup}) do
-    ev.Signal.new(
-      unloop,
-    sig):start(ev.Loop.default)
-  end
-  
-  local sigpipe = 13
-  ev.Signal.new(
-    function()
-      print('SIGPIPE ignored')
-    end,
-  sigpipe):start(ev.Loop.default)
-  
-  ev.Loop.default:loop()
-end
 
 return {
   socket = socket,
--- src/nodish/process.lua
+++ src/nodish/process.lua
@@ -1,4 +1,7 @@
 local ev = require'ev'
+local S = require'syscall'
+local emitter = require'nodish.emitter'
+local stream = require'nodish.stream'
 local tinsert = table.insert
 
 local pcall_array = function(arr)
@@ -45,7 +48,61 @@ local create_nexttick = function(loop)
   end
 end
 
+local stdin = function()
+  local self = emitter.new()
+  self.watchers = {}
+  stream.readable(self)
+  S.stdin:nonblock(true)
+  self._read = function()
+    return S.stdin:read()
+  end
+  self:add_read_watcher(S.stdin:getfd())
+  return self
+end
+
+local stdout = function()
+  local self = emitter.new()
+  self.watchers = {}
+  stream.writable(self)
+  S.stdout:nonblock(true)
+  self._write = function(_,data)
+    return S.stdout:write(data)
+  end
+  self:add_write_watcher(S.stdout:getfd())
+  return self
+end
+
+local unloop = function()
+  print('quitting')
+  ev.Loop.default:unloop()
+end
+
+local loop = function()
+  local sigkill = 9
+  local sigint = 2
+  local sigquit = 3
+  local sighup = 1
+  for _,sig in ipairs({sigkill,sigint,sigquit,sighup}) do
+    ev.Signal.new(
+      unloop,
+    sig):start(ev.Loop.default)
+  end
+  
+  local sigpipe = 13
+  ev.Signal.new(
+    function()
+      print('SIGPIPE ignored')
+    end,
+  sigpipe):start(ev.Loop.default)
+  
+  ev.Loop.default:loop()
+end
+
 return {
   new = create_nexttick,
   nexttick = create_nexttick(ev.Loop.default),
+  stdin = stdin(),
+  stdout = stdout(),
+  loop = loop,
+  unloop = unloop,
 }
