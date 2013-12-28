f3bc1874528821a6bcc7395b9f67ad346251d587
Gerhard Preuss <gelipp@gmail.com>
2013-12-28 22:43:13 +0100

    use pipe interface

--- example/echo_client.lua
+++ example/echo_client.lua
@@ -1,23 +1,13 @@
-#!/usr/bin/env lua
+#!/usr/bin/env luajit
 local this_dir = arg[0]:match('(.+/)[^/]+%.lua') or './'
 package.path = this_dir..'../src/'..package.path
 
 local net = require'nodish.net'
 local process = require'nodish.process'
-local ev = require'ev'
 
 local client = net.connect(12345)
-client:on('connect',function()
-    client:set_nodelay(true)
-    client:on('data',function(data)
-        print('->',data)
-      end)
-    local i = 0
-    ev.Timer.new(function()
-        i = i + 1
-        print('<-',i)
-        client:write(tostring(i)..'\n')
-      end,1,1):start(ev.Loop.default)
-  end)
+
+process.stdin:resume()
+process.stdin:pipe(client)
 
 process.loop()
